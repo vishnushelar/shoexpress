887d05fd31eebf0940877fbd1fdae6f069e8c7ee
Vishnu Shelar <Vishnu Shelar>
2016-03-08 17:58:53 +0530

    mm

--- www/js/constants/constants.js
+++ www/js/constants/constants.js
@@ -1,7 +1,7 @@
 (function () {
   angular.module('mttapp.constants', [])
     .constant('urls', {
-      appId: '145034519587836600',
+      appId: '145632223293769649',
       parse_applicationId: "IT4FXY3YlUtv2KcpKuBvLH9tJFmSFeKnhzcYEMIh",
       parse_clientKey: "JCkQcozxFGVSfR2kFXcl6M4PaQlJM7ckERWbVKZW",
       parse_javaScriptKey: "3mIDqKrZyzEjuTt4CnPc3FsJXGMX8uMNIYSaOcpv",
